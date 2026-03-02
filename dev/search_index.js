6b51be618dabfeda1405e763c5b618d3d218f25b
Documenter.jl <documenter@juliadocs.github.io>
2026-03-02 15:03:22 +0000

    build based on b11faae

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/#API","page":"API Reference","title":"API","text":"","category":"section"},{"location":"theory/#Theory","page":"Theory (Background)","title":"Theory","text":"","category":"section"}]
+}
