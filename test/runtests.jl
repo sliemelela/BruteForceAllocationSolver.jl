ec735a6d3575aa7c183182f914145e40075508cc
Sliem el Ela <sliemelela@gmail.com>
2026-03-07 15:31:20 +0100

    Made test more strict to check all times and wealths

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,10 +7,14 @@ using Test
     γ = 5.0
     u(x) = (x^(1 - γ))/(1 - γ)
 
-    W_grid = exp.(collect(range(log(1.0), log(100.0), length=500)))
+    G_w = 500
+    W_grid = exp.(collect(range(log(1.0), log(100.0), length=G_w)))
     Z_grids = Vector{Float64}[]
     c_grid = collect(range(0.01, 0.99, length=50))
     omega_space = [[w] for w in range(0.0, 1.0, length=101)]
+
+    M = 10
+    β = 0.96
 
     nodes, weights = gausshermite(10)
     ε_nodes = [[n * sqrt(2.0)] for n in nodes]
@@ -25,7 +29,7 @@ using Test
     V, pol_c, pol_w = solve_dynamic_program(
         W_grid, Z_grids, c_grid, omega_space,
         ε_nodes, W_weights, merton_transition,
-        10, 0.96, u, fractional_consumption,
+        M, β, u, fractional_consumption,
         standard_budget_constraint, crra_extrapolator
     )
 
@@ -34,9 +38,11 @@ using Test
 
     # Check if the middle of the grid matches the analytical solution
     numerical_w = pol_w[50, 1][1]
-    display(pol_w)
 
-    @test isapprox(numerical_w, analytical_w, atol=0.02)
+    for W_idx in range(1, G_w), n in range(1, M)
+        numerical_w = pol_w[W_idx, n][1]
+        @test isapprox(numerical_w, analytical_w, atol=0.02)
+    end
 end
 
 @testset "Merton Log-Wealth Benchmark Test" begin
@@ -49,9 +55,11 @@ end
     # 2. The Log-Space Grid
     # Instead of an exponential grid for W, we just use a linear grid for X!
     # log(0.01) ≈ -4.6, log(100.0) ≈ 4.6
-    X_grid = collect(range(log(0.01), log(100.0), length=500))
+    G_X = 500
+    X_grid = collect(range(log(0.01), log(100.0), length=G_X))
     Z_grids = Vector{Float64}[]
-
+    M = 10
+    β = 0.96
     c_grid = collect(range(0.01, 0.99, length=50))
     omega_space = [[w] for w in range(0.0, 1.0, length=101)]
 
@@ -70,17 +78,15 @@ end
     V, pol_c, pol_w = solve_dynamic_program(
         X_grid, Z_grids, c_grid, omega_space,
         ε_nodes, X_weights, merton_transition,
-        10, 0.96, u, log_fractional_consumption,
+        M, β, u, log_fractional_consumption,
         log_budget_constraint, log_extrapolator
     )
 
     # 7. Validate
     analytical_w = (0.07 - 0.02) / (5.0 * 0.20^2) # 0.25
-    numerical_w = pol_w[250, 1][1]
-    numerical_w_2 = pol_w[490, 1][1]
 
-    display(pol_w)
-
-    @test isapprox(numerical_w, analytical_w, atol=0.02)
-    @test isapprox(numerical_w_2, analytical_w, atol=0.02)
+    for X_idx in range(1, G_X), n in range(1, M)
+        numerical_w = pol_w[X_idx, n][1]
+        @test isapprox(numerical_w, analytical_w, atol=0.02)
+    end
 end
