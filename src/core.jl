c9bb89d06c910c27be2d69c7cb45291fa384b796
Sliem el Ela <sliemelela@gmail.com>
2026-03-25 10:49:49 +0100

    Split up tests and add logic for terminal wealth optimization

--- src/core.jl
+++ src/core.jl
@@ -65,6 +65,31 @@ function evaluate_bellman_objective(
     return current_utility + β * expected_future_value
 end
 
+function evaluate_bellman_objective(
+    W_n::Float64, Z_n::Vector{Float64}, ω_n::Vector{Float64},
+    ε_nodes::Vector{Vector{Float64}}, W_weights::Vector{Float64},
+    V_next, transition_model::Function,
+    budget_constraint::Function, extrapolator::Function
+)
+    expected_future_value = 0.0
+
+    for j in 1:length(ε_nodes)
+        ε_j = ε_nodes[j]
+        weight = W_weights[j]
+
+        # 1. Market Transitions
+        Z_next, R_e, R_base = transition_model(Z_n, ε_j)
+
+        # 2. Wealth Transition (Injecting c_n = 0.0)
+        W_next = budget_constraint(W_n, 0.0, ω_n, R_e, R_base)
+
+        # 3. Value Evaluation
+        expected_future_value += weight * extrapolator(W_next, Z_next, V_next)
+    end
+
+    # Return pure expected future value
+    return expected_future_value
+end
 
 """
     optimize_controls_brute_force(
@@ -119,6 +144,30 @@ function optimize_controls_brute_force(
     return best_val, best_c, best_ω
 end
 
+function optimize_controls_brute_force(
+    W_n::Float64, Z_n::Vector{Float64}, omega_space,
+    ε_nodes::Vector{Vector{Float64}}, W_weights::Vector{Float64},
+    V_next, transition_model::Function,
+    budget_constraint::Function, extrapolator::Function
+)
+    best_val = -Inf
+    best_ω = first(omega_space)
+
+    for ω_n in omega_space
+        current_val = evaluate_bellman_objective(
+            W_n, Z_n, ω_n, ε_nodes, W_weights,
+            V_next, transition_model,
+            budget_constraint, extrapolator
+        )
+
+        if current_val > best_val
+            best_val = current_val
+            best_ω = ω_n
+        end
+    end
+
+    return best_val, best_ω
+end
 
 """
     solve_dynamic_program(
@@ -204,4 +253,49 @@ function solve_dynamic_program(
 
     println("Recursion complete.")
     return V, pol_c, pol_w
+end
+
+function solve_dynamic_program(
+    W_grid::Vector{Float64}, Z_grids::Vector{Vector{Float64}},
+    omega_space::Vector{Vector{Float64}},
+    ε_nodes::Vector{Vector{Float64}}, W_weights::Vector{Float64},
+    transition_model::Function, M::Int, u::Function,
+    budget_constraint::Function, extrapolator::Function
+)
+    sz = (length(W_grid), (length(z) for z in Z_grids)...)
+    V     = zeros(Float64, sz..., M + 1)
+    pol_w = Array{Vector{Float64}}(undef, sz..., M)
+
+    println("Setting terminal conditions (Terminal Wealth)...")
+    for idx in CartesianIndices(sz)
+        state_terminal = W_grid[idx[1]]
+        V[idx, M+1] = u(state_terminal)
+    end
+
+    println("Starting backwards recursion from step $M down to 1...")
+    for n in M:-1:1
+        println("  Solving timestep: $n")
+
+        V_next_data = selectdim(V, ndims(V), n + 1)
+        V_next_interp = linear_interpolation(
+            (W_grid, Z_grids...), V_next_data, extrapolation_bc=Line()
+        )
+
+        Threads.@threads for idx in CartesianIndices(sz)
+            W_n = W_grid[idx[1]]
+            Z_n = [Z_grids[k][idx[1+k]] for k in 1:length(Z_grids)]
+
+            best_val, best_ω = optimize_controls_brute_force(
+                W_n, Z_n, omega_space, ε_nodes, W_weights,
+                V_next_interp, transition_model,
+                budget_constraint, extrapolator
+            )
+
+            V[idx, n]     = best_val
+            pol_w[idx, n] = best_ω
+        end
+    end
+
+    println("Recursion complete.")
+    return V, pol_w
 end
